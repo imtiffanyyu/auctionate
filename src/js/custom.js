d5d3a044ed29ce2b8862d0d17dacce0ad21e540a
Tiffany Yu <tiffanyayu@gmail.com>
2016-06-24 17:51:21 +0200

    Added update and delete for bidders

--- src/js/custom.js
+++ src/js/custom.js
@@ -12,12 +12,13 @@ $(document).ready(function(){
 	$(".dropdown-button").dropdown({ beloworigin: true });
 	$('.modal-trigger').leanModal();
 
-	$("#consignors").dataTable( {
-		"lengthChange": false
-	});
-	$("#items").dataTable( {
-		"lengthChange": false
-	});
+	// $("#consignors").dataTable( {
+	// 	"lengthChange": false
+	// });
+	// $("#items").dataTable( {
+	// 	"lengthChange": false
+	// });
+
 
 
 	$("button#addconsignor").click(function() {
@@ -301,4 +302,93 @@ $(document).ready(function(){
 			}
 		});
 	});
+
+	$(".editbidder").click(function() {
+		$("#editbidderform").fadeIn('slow');
+
+			var clickedbidder = $(".clickedbidderid", this).text() // looking for clickedbidderid in the context of this (the thing I clicked)
+			console.log("You clicked bidder " + clickedbidder)
+
+			$.ajax({
+				method: "GET",
+				url: "/bidderjson",
+				data: {
+					clickedbidder: clickedbidder
+				}
+			}).done(function(bidder){
+				console.log(bidder)
+				$('#displaybidderid').text (bidder.id)
+				$('#firstname').text (bidder.firstname)
+				$('#lastname').text (bidder.lastname)
+				$('#address').text (bidder.address)
+				$('#zipcode').text (bidder.zipcode)
+				$('#city').text (bidder.city)
+				$('#country').text (bidder.country)
+				$('#phone').text (bidder.phone)
+				$('#email').text (bidder.email)
+				$('#payment').text (bidder.payment)
+				$('#shipping').text (bidder.shipping)
+			})
+
+
+		})
+
+	$(".deletebidder").click(function() {
+		var deletebidderid = $("#displaybidderid").text();
+		console.log(deletebidderid);
+
+		$.ajax({
+			method: "DELETE",
+			url: "/bidder",
+			data: {
+				deletebidderid: deletebidderid
+			}
+		})
+	})
+
+	$(".beditlink").on("click", function(e){
+		e.preventDefault();
+		var dataset = $(this).prev(".datainfo");
+		var savebtn = $(this).next(".bsavebtn");
+		var theid   = dataset.attr("id");
+		var newid   = theid+"-form";
+		var currval = dataset.text();
+
+		dataset.empty();
+
+		$('<input type="text" name="'+newid+'" id="'+newid+'" value="'+currval+'" class="hlite">').appendTo(dataset);
+
+		$(this).css("display", "none");
+		savebtn.css("display", "block");
+	});
+
+	$(".bsavebtn").on("click", function(e){
+		e.preventDefault();
+
+		var displaybidderid = $("#displaybidderid").text();
+		console.log(displaybidderid);
+
+			var elink   = $(this).prev(".beditlink"); // looks for "editlink" before savebtn
+			var dataset = elink.prev(".datainfo"); // looks for "datainfo" before editlink
+			var newid   = dataset.attr("id"); // grabs id for form creaton
+			var cinput  = "#"+newid+"-form"; // creates form for the id
+			var einput  = $(cinput);
+			var newval  = einput.val();
+			console.log('form value is: ' + newval)
+			$(this).css("display", "none");
+			einput.remove();
+			dataset.html(newval); // Supposed to update the values
+			elink.css("display", "block");
+
+
+			$.ajax({
+				method: "PUT",
+				url: "/bidder",
+				data: {
+					newid: newid,
+					newValue: newval,
+					displaybidderid: displaybidderid
+				}
+			})
+		});
 });
